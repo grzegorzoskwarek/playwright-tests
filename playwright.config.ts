f8ef2b91b121a4f3fb588775f14fc94d78508833
gr33gor <oskwarek.grzegorz@gmail.com>
2025-07-26 14:22:32 +0200

    Set test timeout to 10s and enabled traces always on

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,8 +18,9 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  timeout: 10000,
   use: {
-    trace: 'on-first-retry',
+    trace: 'on',
   },
   projects: [
     {
