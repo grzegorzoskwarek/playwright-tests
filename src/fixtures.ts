512ec5f08f17c28647767cce5cdea3b254bab3be
gr33gor <oskwarek.grzegorz@gmail.com>
2025-07-27 16:48:08 +0200

    refactor: move Page Objects and fixtures to src, clean up old files and folders

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -1,25 +1,25 @@
 import { test as base } from '@playwright/test';
-import { HomePage } from '../tests/pages/HomePage';
-import { RegisterPage } from '../tests/pages/RegisterPage';
-import { LoginPage } from '../tests/pages/LoginPage';
-import { EventsPage } from '../tests/pages/EventsPage';
+import { HomePage } from './pages/HomePage';
+import { RegisterPage } from './pages/RegisterPage';
+import { LoginPage } from './pages/LoginPage';
+import { EventsPage } from './pages/EventsPage';
 
 export const test = base.extend<{
-  homePage: HomePage;
-  registerPage: RegisterPage;
-  loginPage: LoginPage;
-  eventsPage: EventsPage;
+    homePage: HomePage;
+    registerPage: RegisterPage;
+    loginPage: LoginPage;
+    eventsPage: EventsPage;
 }>({
-  homePage: async ({ page }, use) => {
-    await use(new HomePage(page));
-  },
-  registerPage: async ({ page }, use) => {
-    await use(new RegisterPage(page));
-  },
-  loginPage: async ({ page }, use) => {
-    await use(new LoginPage(page));
-  },
-  eventsPage: async ({ page }, use) => {
-    await use(new EventsPage(page));
-  },
+    homePage: async ({ page }, use) => {
+        await use(new HomePage(page));
+    },
+    registerPage: async ({ page }, use) => {
+        await use(new RegisterPage(page));
+    },
+    loginPage: async ({ page }, use) => {
+        await use(new LoginPage(page));
+    },
+    eventsPage: async ({ page }, use) => {
+        await use(new EventsPage(page));
+    },
 });
